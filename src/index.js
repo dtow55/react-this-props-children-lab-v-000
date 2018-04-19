cf0071bf9ae5f9dc8e4f0ac681b08155f40a2244
David Towster <djt4xe@virginia.edu>
2018-04-19 15:36:18 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -4,9 +4,12 @@ import ReactDOM from 'react-dom';
 import Invitation from './components/Invitation';
 import ThemedParty from './components/ThemedDecorations';
 
+const INVITATION_CONTENT = <p>Come to this awesome party!</p>;
+
 ReactDOM.render(
   <div>
-    <Invitation>Hey, you should totally come to this awesome party I'm throwing!</Invitation>
+    <Invitation>Hey, you should totally come to this awesome party I'm throwing!
+    </Invitation>
     <ThemedParty theme="heaven">
       <p>A decoration</p>
       <p>Some other decoration</p>
